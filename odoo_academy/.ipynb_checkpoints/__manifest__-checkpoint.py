b20a9ce63eb8bdc3b7c1f75f8f94bb8d385d334a
ghostinfernalejo <joe.kofisallc@gmail.com>
2021-09-16 19:05:55 +0000

    [ADD] security module verified

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -22,11 +22,13 @@
     'depends': ['base'],
     
     'data': [
-        
+        'views/academy_menuitems.xml',
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
     ],
     
     'demo': [
-        
+        'demo/academy_demo.xml',
     ],
     
 }
---
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -0,0 +1,22 @@
+# -*- coding :utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    
+    _name = 'academy.course'
+    _description = 'Course Info'
+    
+    name = fields.Char(string ='Title', required=True)
+    description = fields.Text(string ='Description')
+    
+    level = fields.Selection(
+            string='level',
+            selection= [
+                ('beginner','Beginner'),
+                ('intermediate','Intermediate'),
+                ('advanced', 'Advanced')
+            ], copy=False
+    )
+    
+    active = fields.Boolean(string='Active', default=True)
