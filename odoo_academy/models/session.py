57cea9c449a3115e12f854d128c2aa253d92d2ec
ghostinfernalejo <joe.kofisallc@gmail.com>
2021-09-25 10:24:46 +0000

    [ADD] Module 8 complete

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -28,6 +28,11 @@ class Session(models.Model):
                           inverse='_inverse_end_date',
                           store=True)
     
+
+    
+    total_price = fields.Float(string='Total price',
+                               related='course_id.total_price')
+    
     @api.depends('start_date', 'duration')
     def _compute_end_date(self):
         for record in self:
